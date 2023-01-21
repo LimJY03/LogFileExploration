bdc6d9d0c86227afbf55ea1f1479a85af2d865d9
Lim JY <limjunyi03@gmail.com>
2023-01-21 08:00:28 +0000

    Code Optimization

--- FindPartitionStats.java
+++ FindPartitionStats.java
@@ -5,31 +5,10 @@ import java.io.FileOutputStream;
 import java.io.PrintWriter;
 
 import java.util.Scanner;
-import java.util.LinkedHashMap;
 
 public class FindPartitionStats {
     
-    private int cpuOpteronCount = 0, cpuEpycCount = 0, gpuK10Count = 0, gpuK40cCount = 0, gpuTitanCount = 0, gpuV100sCount = 0;
-    
-    public static void main(String[] args) {
-        new FindPartitionStats().exec();
-    } 
-    
-    public LinkedHashMap<String, Integer> getPartitionStats() {
-
-        this.exec();
-
-        LinkedHashMap<String, Integer> arr = new LinkedHashMap<>();
-        
-        arr.put("cpu-opteron", this.cpuOpteronCount);
-        arr.put("cpu-epyc", this.cpuEpycCount);
-        arr.put("gpu-k10", this.gpuK10Count);
-        arr.put("gpu-k40c", this.gpuK40cCount);
-        arr.put("gpu-titan", this.gpuTitanCount);
-        arr.put("gpu-v100s", this.gpuV100sCount);
-
-        return arr;
-    }
+    public static void main(String[] args) { new FindPartitionStats().exec(); } 
 
     private void exec() {
 
@@ -37,27 +16,29 @@ public class FindPartitionStats {
 
         try{
             
-            Scanner inputStream = new Scanner(new FileInputStream("./data/extracted_log"));
+            Scanner inputStream = new Scanner(new FileInputStream("./data_filtered/start_job.txt"));
             PrintWriter outputStream = new PrintWriter(new FileOutputStream("./stats/partition_stats.txt"));
+
+            int cpuOpteronCount = 0, cpuEpycCount = 0, gpuK10Count = 0, gpuK40cCount = 0, gpuTitanCount = 0, gpuV100sCount = 0;
 
             while (inputStream.hasNextLine()) {
 
                 line = inputStream.nextLine();
 
-                if (line.contains("sched") && line.contains("cpu-opteron")) { this.cpuOpteronCount++; }
-                else if (line.contains("sched") && line.contains("cpu-epyc")) { this.cpuEpycCount++; }
-                else if (line.contains("sched") && line.contains("gpu-k10")) { this.gpuK10Count++; }
-                else if (line.contains("sched") && line.contains("gpu-k40c")) { this.gpuK40cCount++; }
-                else if (line.contains("sched") && line.contains("gpu-titan")) { this.gpuTitanCount++; }
-                else if (line.contains("sched") && line.contains("gpu-v100s")) { this.gpuV100sCount++; }
+                if (line.contains("cpu-opteron")) { cpuOpteronCount++; }
+                else if (line.contains("cpu-epyc")) { cpuEpycCount++; }
+                else if (line.contains("gpu-k10")) { gpuK10Count++; }
+                else if (line.contains("gpu-k40c")) { gpuK40cCount++; }
+                else if (line.contains("gpu-titan")) { gpuTitanCount++; }
+                else if (line.contains("gpu-v100s")) { gpuV100sCount++; }
             }
 
-            outputStream.println("cpu-opteron: " + this.cpuOpteronCount);
-            outputStream.println("cpu-epyc: " + this.cpuEpycCount);
-            outputStream.println("gpu-k10: " + this.gpuK10Count);
-            outputStream.println("gpu-k40c: " + this.gpuK40cCount);
-            outputStream.println("gpu-titan: " + this.gpuTitanCount);
-            outputStream.println("gpu-v100s: " + this.gpuV100sCount);
+            outputStream.println("cpu-opteron: " + cpuOpteronCount);
+            outputStream.println("cpu-epyc: " + cpuEpycCount);
+            outputStream.println("gpu-k10: " + gpuK10Count);
+            outputStream.println("gpu-k40c: " + gpuK40cCount);
+            outputStream.println("gpu-titan: " + gpuTitanCount);
+            outputStream.println("gpu-v100s: " + gpuV100sCount);
             
             inputStream.close();
             outputStream.close();
