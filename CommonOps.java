d817a591bc4a25f26f346d7e71b67235c05441b0
Lim JY <limjunyi03@gmail.com>
2023-01-21 08:05:41 +0000

    Import Used-Only Modules

--- CommonOps.java
+++ CommonOps.java
@@ -1,6 +1,6 @@
 // Import necessary dependencies
 import java.util.Collections;
-import java.util.Map;
+import java.util.Map.Entry;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,11 +9,11 @@ public class CommonOps {
     
     public LinkedHashMap<String, Integer> sortDescending(LinkedHashMap<String, Integer> dict) {
 
-        List<Map.Entry<String, Integer>> list = new LinkedList<>(dict.entrySet());
+        List<Entry<String, Integer>> list = new LinkedList<>(dict.entrySet());
         Collections.sort(list, (map1, map2) -> map2.getValue().compareTo(map1.getValue()));
         
         LinkedHashMap<String, Integer> newDict = new LinkedHashMap<>();
-        for (Map.Entry<String, Integer> map: list) { newDict.put(map.getKey(), map.getValue()); }
+        for (Entry<String, Integer> map: list) { newDict.put(map.getKey(), map.getValue()); }
 
         return newDict;
     }
