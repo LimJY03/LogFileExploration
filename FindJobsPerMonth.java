8ca15a14de361fb62d037bce6a84527c8c9649bb
Lim JY <limjunyi03@gmail.com>
2023-01-22 01:29:15 +0000

    Improve Code Readability

--- FindJobsPerMonth.java
+++ FindJobsPerMonth.java
@@ -24,34 +24,18 @@ public class FindJobsPerMonth {
             PrintWriter fileOut = new PrintWriter(new FileWriter("./stats/jobs_per_month.txt"), true);
 
             while ((line = startFileIn.readLine()) != null) {
-                if (line.contains("JobId=")) {
-                    switch (line.split("-")[1]) {
-                        case "06" -> table[0].addStart();
-                        case "07" -> table[1].addStart();
-                        case "08" -> table[2].addStart();
-                        case "09" -> table[3].addStart();
-                        case "10" -> table[4].addStart();
-                        case "11" -> table[5].addStart();
-                        case "12" -> table[6].addStart();
-                        default -> {}
-                    }
-                }
+                if (line.contains("JobId=")) { new CustomTable().distribute(table, line, "x"); }
             }
             
             while ((line = endFileIn.readLine()) != null) {
-                switch (line.split("-")[1]) {
-                    case "06" -> table[0].addEnd();
-                    case "07" -> table[1].addEnd();
-                    case "08" -> table[2].addEnd();
-                    case "09" -> table[3].addEnd();
-                    case "10" -> table[4].addEnd();
-                    case "11" -> table[5].addEnd();
-                    case "12" -> table[6].addEnd();
-                    default -> {}
-                }
+                if (line.contains("JobId=")) { new CustomTable().distribute(table, line, "y"); }
             }
 
-            for (CustomTable monthData : table) { fileOut.println(monthData.toString()); }
+            for (CustomTable monthData : table) { 
+                monthData.set_xlabel("started"); 
+                monthData.set_ylabel("ended"); 
+                fileOut.println(monthData.toString()); 
+            }
 
             startFileIn.close();
             endFileIn.close();
@@ -60,20 +44,4 @@ public class FindJobsPerMonth {
         catch (FileNotFoundException e) { System.out.println("File was not found."); } 
         catch (IOException e) { System.out.println("Error read from file."); }
     }
-}
-
-class CustomTable {
-
-    private String month;
-    private int start, end;
-
-    public CustomTable(String month, int start, int end) {
-        this.month = month;
-        this.start = start;
-        this.end = end;
-    }
-
-    public void addStart() { this.start++; }
-    public void addEnd() { this.end++; }
-    public String toString() { return String.format("%s: %d started, %d ended", this.month, this.start, this.end); }
 }
