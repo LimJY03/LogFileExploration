fd093f82e4f2e18ad833c709b5f4821ad1d4bf8f
Lim JY <limjunyi03@gmail.com>
2023-01-23 03:29:40 +0000

    Fixed Logic Error

--- FindPartitionStats.java
+++ FindPartitionStats.java
@@ -22,6 +22,7 @@ public class FindPartitionStats {
 
             int cpuOpteronCount = 0, cpuEpycCount = 0, gpuK10Count = 0, gpuK40cCount = 0, gpuTitanCount = 0, gpuV100sCount = 0;
             long cpuOpteron_totalTime = 0, cpuEpyc_totalTime = 0, gpuK10_totalTime = 0, gpuK40c_totalTime = 0, gpuTitan_totalTime = 0, gpuV100s_totalTime = 0;
+            int cpuOpteron_completedCount = 0, cpuEpyc_completedCount = 0, gpuK10_completedCount = 0, gpuK40c_completedCount = 0, gpuTitan_completedCount = 0, gpuV100s_completedCount = 0;
 
             while (inputStream.hasNextLine()) {
 
@@ -39,20 +40,20 @@ public class FindPartitionStats {
 
                 line = inputNodeData.nextLine();
 
-                if (line.contains("cpu-opteron")) { cpuOpteron_totalTime += Long.parseLong(line.split(" ")[1]); }
-                else if (line.contains("cpu-epyc")) { cpuEpyc_totalTime += Long.parseLong(line.split(" ")[1]); }
-                else if (line.contains("gpu-k10")) { gpuK10_totalTime += Long.parseLong(line.split(" ")[1]); }
-                else if (line.contains("gpu-k40c")) { gpuK40c_totalTime += Long.parseLong(line.split(" ")[1]); }
-                else if (line.contains("gpu-titan")) { gpuTitan_totalTime += Long.parseLong(line.split(" ")[1]); }
-                else if (line.contains("gpu-v100s")) { gpuV100s_totalTime += Long.parseLong(line.split(" ")[1]); }
+                if (line.contains("cpu-opteron")) { cpuOpteron_totalTime += Long.parseLong(line.split(" ")[1]); cpuOpteron_completedCount++; }
+                else if (line.contains("cpu-epyc")) { cpuEpyc_totalTime += Long.parseLong(line.split(" ")[1]); cpuEpyc_completedCount++; }
+                else if (line.contains("gpu-k10")) { gpuK10_totalTime += Long.parseLong(line.split(" ")[1]); gpuK10_completedCount++; }
+                else if (line.contains("gpu-k40c")) { gpuK40c_totalTime += Long.parseLong(line.split(" ")[1]); gpuK40c_completedCount++; }
+                else if (line.contains("gpu-titan")) { gpuTitan_totalTime += Long.parseLong(line.split(" ")[1]); gpuTitan_completedCount++; }
+                else if (line.contains("gpu-v100s")) { gpuV100s_totalTime += Long.parseLong(line.split(" ")[1]); gpuV100s_completedCount++; }
             }
 
-            outputStream.printf("cpu-opteron: %-4s jobs executed, used %s ms on average.\n", cpuOpteronCount, cpuOpteron_totalTime / cpuOpteronCount);
-            outputStream.printf("cpu-epyc:    %-4s jobs executed, used %s ms on average.\n", cpuEpycCount, cpuEpyc_totalTime / cpuEpycCount);
-            outputStream.printf("gpu-k10:     %-4s jobs executed, used %s ms on average.\n", gpuK10Count, gpuK10_totalTime / gpuK10Count);
-            outputStream.printf("gpu-k40c:    %-4s jobs executed, used %s ms on average.\n", gpuK40cCount, gpuK40c_totalTime / gpuK40cCount);
-            outputStream.printf("gpu-titan:   %-4s jobs executed, used %s ms on average.\n", gpuTitanCount, gpuTitan_totalTime / gpuTitanCount);
-            outputStream.printf("gpu-v100s:   %-4s jobs executed, used %s ms on average.\n", gpuV100sCount, gpuV100s_totalTime / gpuV100sCount);
+            outputStream.printf("cpu-opteron: %-4s jobs executed, used %s ms on average.\n", cpuOpteronCount, cpuOpteron_totalTime / cpuOpteron_completedCount);
+            outputStream.printf("cpu-epyc:    %-4s jobs executed, used %s ms on average.\n", cpuEpycCount, cpuEpyc_totalTime / cpuEpyc_completedCount);
+            outputStream.printf("gpu-k10:     %-4s jobs executed, used %s ms on average.\n", gpuK10Count, gpuK10_totalTime / gpuK10_completedCount);
+            outputStream.printf("gpu-k40c:    %-4s jobs executed, used %s ms on average.\n", gpuK40cCount, gpuK40c_totalTime / gpuK40c_completedCount);
+            outputStream.printf("gpu-titan:   %-4s jobs executed, used %s ms on average.\n", gpuTitanCount, gpuTitan_totalTime / gpuTitan_completedCount);
+            outputStream.printf("gpu-v100s:   %-4s jobs executed, used %s ms on average.\n", gpuV100sCount, gpuV100s_totalTime / gpuV100s_completedCount);
             
             inputStream.close();
             inputNodeData.close();
